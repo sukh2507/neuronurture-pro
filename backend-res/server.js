38d020fee8010158ea17cbbeb509464222835902
Sukhbir Singh Sareen <74489389+sukh2507@users.noreply.github.com>
2025-07-29 14:42:19 +0530

    Update server.js

--- backend-res/server.js
+++ backend-res/server.js
@@ -29,7 +29,7 @@ connectDB();
 // Middlewares
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:8080'],
+  origin: ['http://localhost:3000', 'http://localhost:8080','https://neuronurture-pro.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
